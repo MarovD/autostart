a4789ea73d1bf88424fb51033f826a97582c0a0f
Dmitry Marov <d.marov94@gmail.com>
2018-01-09 13:27:37 +0300

    fix

--- autostart.zsh
+++ autostart.zsh
@@ -1,14 +1,23 @@
 #!/usr/local/bin/zsh
 
-#wmctrl -s 1;
-sleep 20;
+sleep 15;
 
 xfce4-terminal --title='vifm' --execute 'vifm' &;
 sleep 3;
-wmctrl -r 'vifm' -t 0;
-wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
+wmctrl -r vifm -t 0;
+wmctrl -r vifm -b toggle,maximized_horz,maximized_vert;
+
+thunderbird &;
+sleep 3;
+wmctrl -r thunderbird -t 1;
 
 firefox &;
 sleep 3;
-wmctrl -r mozilla -t 1;
+wmctrl -r firefox -t 1;
 
+# xfce4-terminal --title='cmus' --execute 'cmus' &;
+# sleep 3;
+# wmctrl -r cmus -t 7;
+# wmctrl -r cmus -b toggle,maximized_horz,maximized_vert;
+
+wmctrl -s 0;
