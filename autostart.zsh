e4a9faa5c3318e6e09558c66c17c87b4bbbde0b6
Dmitry Marov <d.marov94@gmail.com>
2018-01-09 10:04:13 +0300

    work init

--- autostart.zsh
+++ autostart.zsh
@@ -1,13 +1,14 @@
 #!/usr/local/bin/zsh
 
 #wmctrl -s 1;
-sleep 30;
-
-firefox &;
-sleep 3;
-wmctrl -r mozilla -t 0;
+sleep 20;
 
 xfce4-terminal --title='vifm' --execute 'vifm' &;
 sleep 3;
-wmctrl -r 'vifm' -t 1;
+wmctrl -r 'vifm' -t 0;
 wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
+
+firefox &;
+sleep 3;
+wmctrl -r mozilla -t 1;
+
