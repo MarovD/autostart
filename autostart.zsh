c81a1caf7d0c3c6bbb9c73d50495ea3215edaf06
Dmitry Marov <d.marov94@gmail.com>
2018-01-16 10:05:25 +0300

    improvement

--- autostart.zsh
+++ autostart.zsh
@@ -5,12 +5,13 @@ thunderbird &;
 firefox &;
 xfce4-terminal --title='vifm' --execute 'vifm' &;
 
-sleep 20;
+sleep 30;
+
+wmctrl -r 'vifm' -t 0
+wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
 
 wmctrl -r 'thunderbird' -t 1;
 wmctrl -r 'firefox' -t 1;
-wmctrl -r 'vifm' -t 0
-wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
 
 wmctrl -s 0;
 
