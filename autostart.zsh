c05819657bc4ffab2185080846e4fbb64b40fea6
Dmitry Marov <d.marov94@gmail.com>
2018-01-16 19:23:40 +0300

    inmrovement

--- autostart.zsh
+++ autostart.zsh
@@ -1,12 +1,12 @@
 #!/usr/local/bin/zsh
 
 #wmctrl -s 1;
-sleep 30;
+# sleep 20;
 
 firefox &;
 xfce4-terminal --title='vifm' --execute 'vifm' &;
 
-sleep 3;
+sleep 30;
 
 wmctrl -r mozilla -t 0;
 wmctrl -r 'vifm' -t 1;
