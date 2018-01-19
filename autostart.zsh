4e83118ba9ebbad4d35f66dd8d51e58577d77227
Dmitry Marov <d.marov94@gmail.com>
2018-01-19 22:18:42 +0300

    mount added

--- autostart.zsh
+++ autostart.zsh
@@ -2,11 +2,20 @@
 
 #wmctrl -s 1;
 # sleep 10;
+if [ ! -d '/media/disk' ];
+then
+    mkdir '/media/disk';
+fi;
+
+if [ -c '/dev/diskid/DISK-NA8CW7V7p1' ];
+then
+    mount -t ext2fs '/dev/da0p1' '/media/disk'
+fi;
 
 firefox &;
 xfce4-terminal --title='vifm' --execute 'vifm' &;
 
-sleep 20;
+sleep 40;
 
 wmctrl -r mozilla -t 0;
 wmctrl -r 'vifm' -t 1;
