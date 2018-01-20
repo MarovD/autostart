191861e13d1f9083eacdd0b1b5fba392b1c2a9aa
Dmitry Marov <d.marov94@gmail.com>
2018-01-20 09:45:07 +0300

    fix

--- autostart.zsh
+++ autostart.zsh
@@ -1,22 +1,23 @@
 #!/usr/local/bin/zsh
 
-#wmctrl -s 1;
-# sleep 10;
 if [ ! -d '/media/disk' ];
 then
     mkdir '/media/disk';
 fi;
 
-if [ -c '/dev/diskid/DISK-NA8CW7V7p1' ];
+if [ -c '/dev/da0p1' ];
 then
     mount -t ext2fs '/dev/da0p1' '/media/disk'
 fi;
 
 firefox &;
-xfce4-terminal --title='vifm' --execute 'vifm' &;
+thunderbird &;
+${0:a:h}/../scripts/file-manager.zsh;
 
 sleep 40;
 
-wmctrl -r mozilla -t 0;
+wmctrl -r 'thunderbird' -t 0;
+wmctrl -r 'firefox' -t 0;
 wmctrl -r 'vifm' -t 1;
-wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
+# wmctrl -r 'vifm' -b toggle,maximized_horz,maximized_vert;
+wmctrl -s 1;
