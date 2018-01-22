74a45339fc2a090e18599abcb13964e912c7e1c0
Dmitry Marov <d.marov94@gmail.com>
2018-01-22 10:55:08 +0300

    fix

--- autostart.zsh
+++ autostart.zsh
@@ -3,10 +3,11 @@
 thunderbird &;
 firefox &;
 ${0:a:h}/../scripts/file-manager.zsh &;
-${0:a:h}/../scripts/audio-player.zsh &;
 
 sleep 60;
 
 wmctrl -r 'thunderbird' -t 2;
 wmctrl -r 'firefox' -t 1;
 wmctrl -r 'vifm' -t 0
+
+wmctrl -s 0;
