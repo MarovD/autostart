3526ebed5de0e2a93d8cd14872d31021cef5e9e8
User <user@mdpc.websm.io>
2019-01-11 13:12:27 +0300

    update

--- autostart.sh
+++ autostart.sh
@@ -2,12 +2,13 @@
 
 thunderbird &;
 firefox &;
-xfce4-terminal --maximize --command "vifm" -T "vifm";
+konsole -e mc &;
+#xfce4-terminal --maximize --command "vifm" -T "vifm";
 
 sleep 60;
 
 wmctrl -r 'thunderbird' -t 7;
 wmctrl -r 'firefox' -t 4;
-wmctrl -r 'vifm' -t 1;
+wmctrl -r 'mc' -t 1;
 
 wmctrl -s 4;
